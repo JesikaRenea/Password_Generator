823370ea380717c69d7f31da6285decd37bbeaa4
JesikaRenea <JessicaHayesUCF@outlook.com>
2020-01-16 19:21:45 -0500

    Back to original

--- script.js
+++ script.js
@@ -15,36 +15,63 @@ function writePassword() {
 
 
 function generatePassword(){
+  var characters =[]
+
+
+  var symbol = '!@#$%^&*()'.split('')
+  console.log(symbol)
+
+  var number = '0123456789'.split('')
+  console.log(number)
+
+  var lowerChar = 'abcdefghijklmnopqrstuvwxyz'.split('')
+  console.log(lowerChar)
+
+  var upperChar = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('')
+  console.log(upperChar)
 
   var length = prompt("How long do you want your password to be? (Between 8-20 characters")
   console.log (length);
 
-  var comfirmSpecial = confirm("Do you want to include a special character")
-  console.log (comfirmSpecial)
+  if (length === null || length === '' || length < 8 || length > 20){
+    alert("Password must be between 8 and 20 characters");
+    return;
+  }
+
+  var confirmSymbol = confirm("Do you want to include a symbol character")
+  console.log (confirmSymbol)
+    if(confirmSymbol)characters.push(symbol)
 
   var confirmNum = confirm("Do you want to include a numeric character?")
   console.log (confirmNum);
+    if(confirmNum)characters.push(number)
 
   var confirmLower = confirm("Do you want to include a lowercase character?")
   console.log (confirmLower);
+    if(confirmLower)characters.push(lowerChar)
 
   var confirmUpper = confirm("Do you want to include an uppercase character?")
   console.log (confirmUpper);
-  
-  var symbol = '!@#$%^&*()'
-  var number = '0123456789'
-  var lowerChar = 'abcdefghijklmnopqrstuvwxyz'
-  var upperChar = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    if(confirmUpper)characters.push(upperChar)
 
-  var characters = [symbol, number, lowerChar, upperChar]
-  console.log (characters)
-  
-  for (var i = 0; i < length; i++){
-    pwd = characters.charAt(Math.floor(Math.random() * characters.length))}
+console.log(characters)
+
+  if (!confirmSymbol && !confirmNum && !confirmLower && !confirmUpper){
+    alert("Password must contain at least 1 special character");
+    return;
+  }
   
 
+  for( i = 0; i < length; i++){
+    var pwd = characters.charAt(Math.floor(Math.random() * length));
 
-   return "--pwd--"
+
+  }
+
+console.log(pwd)
+
+   return pwd
+
   //needs to display new variable defined by generate password function
 
 }
@@ -57,3 +84,5 @@ function copyToClipboard() {
 generateBtn.addEventListener("click", writePassword);
 
 // BONUS EVENT LISTENER
+
+
